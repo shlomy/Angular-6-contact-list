a1512ffab8c3419950449b2196c12c31bf894838
shlomys <sellashlomy@gmail.com>
2018-07-04 20:15:41 +0300

    search-highlight

--- src/app/highlight.pipe.ts
+++ src/app/highlight.pipe.ts
@@ -1,12 +1,18 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { PipeTransform, Pipe } from '@angular/core';
 
-@Pipe({
-  name: 'highlight'
-})
+@Pipe({ name: 'highlight' })
 export class HighlightPipe implements PipeTransform {
+  transform(text: string, search): string {
+    if (search && text) {
+      let pattern = search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+      pattern = pattern.split(' ').filter((t) => {
+        return t.length > 0;
+      }).join('|');
+      const regex = new RegExp(pattern, 'gi');
 
-  transform(value: any, args?: any): any {
-    return null;
+      return text.replace(regex, (match) => `<span class="search-highlight">${match}</span>`);
+    } else {
+      return text;
+    }
   }
-
 }
