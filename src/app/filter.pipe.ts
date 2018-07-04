49a629e7944a87a90c8e0cb10d7201321238ca2d
shlomys <sellashlomy@gmail.com>
2018-07-04 20:57:08 +0300

    twiks

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -5,13 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, filterString: string , propName:string): any {
-  
-    if(value.length=== undefined || filterString===''){
-     return value
+  transform(value: any, filterString: string, propName: string): any {
+
+    if (value.length === undefined || filterString === '') {
+      return value
     }
-    
-    
+
+
     const result = []
     for (const user of value) {
       if (user.name.first.match(filterString)) {
